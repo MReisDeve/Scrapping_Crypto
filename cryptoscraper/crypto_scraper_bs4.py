fd9cc51801fdbcc97f4930514477221a82a8bcf8
Frouzin <matheusereis44@gmail.com>
2023-07-11 00:09:15 -0300

    Localizando e trabalhando as tags no BeautifulSoup

--- cryptoscraper/crypto_scraper_bs4.py
+++ cryptoscraper/crypto_scraper_bs4.py
@@ -5,6 +5,7 @@ url = "https://coinmarketcap.com/"
 web_page = requests.get(url)
 html_document = web_page.text
 soup = BeautifulSoup(html_document, "html.parser")
+crypto_table = soup.find(name="table", attrs="sc-996d6db8-3 cOXNvh cmc-table")
 
 # print(type(web_page))
 # print(web_page)

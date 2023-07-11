3efee3abe8a1c00123c486fbcb5a76871474d147
Frouzin <matheusereis44@gmail.com>
2023-07-11 19:34:51 -0300

    Atualização com scrapping de dados do site

--- cryptoscraper/crypto_scraper_bs4.py
+++ cryptoscraper/crypto_scraper_bs4.py
@@ -5,9 +5,18 @@ url = "https://coinmarketcap.com/"
 web_page = requests.get(url)
 html_document = web_page.text
 soup = BeautifulSoup(html_document, "html.parser")
-crypto_table = soup.find(name="table", attrs="sc-996d6db8-3 cOXNvh cmc-table")
 
-# print(type(web_page))
-# print(web_page)
-# print(dir(web_page))
-# print(web_page.text)
+
+crypto_table_body = soup.tbody
+row = crypto_table_body.tr
+_, _, name, price, one_hr, one_day, one_week, market_cap, volume, circ_supply, week, _ =row.find_all(name="td")
+
+print(name.p.text)
+print(price.span.text)
+print(round(float(one_hr.span.text.strip("%")),2))
+print(round(float(one_day.span.text.strip("%")),2))
+print(round(float(one_week.span.text.strip("%")),2))
+print(market_cap.find(name= "span", attrs={"data-nosnippet":"true"}).text)
+print(volume.p.text)
+print(circ_supply.p.text)
+print(f"{url}{week.a.get('href')}")

d3a1c843768ccc8600245e784ca1ad61ae9ee73c
Frouzin <matheusereis44@gmail.com>
2023-07-11 23:22:56 -0300

    utilização do BeutifulSoup com sucesso

--- cryptoscraper/crypto_scraper_bs4.py
+++ cryptoscraper/crypto_scraper_bs4.py
@@ -1,22 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://coinmarketcap.com/"
+url = "https://coinmarketcap.com"
 web_page = requests.get(url)
 html_document = web_page.text
 soup = BeautifulSoup(html_document, "html.parser")
-
-
 crypto_table_body = soup.tbody
-row = crypto_table_body.tr
-_, _, name, price, one_hr, one_day, one_week, market_cap, volume, circ_supply, week, _ =row.find_all(name="td")
 
-print(name.p.text)
-print(price.span.text)
-print(round(float(one_hr.span.text.strip("%")),2))
-print(round(float(one_day.span.text.strip("%")),2))
-print(round(float(one_week.span.text.strip("%")),2))
-print(market_cap.find(name= "span", attrs={"data-nosnippet":"true"}).text)
-print(volume.p.text)
-print(circ_supply.p.text)
-print(f"{url}{week.a.get('href')}")
+table = []
+for tr in crypto_table_body.contents:
+    try:
+        _, _, name, price, one_hr, one_day, one_week, market_cap, volume, circ_supply, week_chart, _ =tr.contents
+    except ValueError:
+        break
+    processed_row = {
+        'name' : name.p.text,
+        'price' : price.span.text,
+        'one_hr': round(float(one_hr.span.text.strip("%")),2),
+        'one_day': round(float(one_day.span.text.strip("%")),2),
+        'one_week': round(float(one_week.span.text.strip("%")),2),
+        'market_cap':market_cap.find(name= "span", attrs={"data-nosnippet":"true"}).text,
+        'volume': volume.p.text,
+        'circ_supply': circ_supply.p.text,
+        'week_chart': f"{url}{week_chart.a.get('href')}",
+        
+    }
+    table.append(processed_row)
+    for crypto in table:
+        print(crypto)
